277f979b6eed5d42eb8480f9a684039758236224
JooYoo <jooyoo@outlook.com>
2020-05-28 21:16:17 +0200

    added about

--- src/.vuepress/components/home/Home.vue
+++ src/.vuepress/components/home/Home.vue
@@ -2,44 +2,24 @@
   <div>
     <yuHeader></yuHeader>
 
-    <v-card class="mx-auto" max-width="344">
-      <v-img src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg" height="200px"></v-img>
+    <v-flex class="container--about">
+      <yuAbout></yuAbout>
+    </v-flex>
 
-      <v-card-title>Top western road trips</v-card-title>
-
-      <v-card-subtitle>1,000 miles of wonder</v-card-subtitle>
-
-      <v-card-actions>
-        <v-btn text>Share</v-btn>
-
-        <v-btn color="purple" text>Explore</v-btn>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon @click="show = !show">
-          <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
-        </v-btn>
-      </v-card-actions>
-
-      <v-expand-transition>
-        <div v-show="show">
-          <v-divider></v-divider>
-
-          <v-card-text>I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.</v-card-text>
-        </div>
-      </v-expand-transition>
-    </v-card>
-
-    <yuFooter></yuFooter>
+    <v-flex class="container--footer">
+      <yuFooter></yuFooter>
+    </v-flex>
   </div>
 </template>
 
 <script>
-import yuHeader from "./YuHeader";
-import yuFooter from "./YuFooter";
+import yuHeader from "./widgets/YuHeader";
+import yuFooter from "./widgets/YuFooter";
+import yuAbout from "./widgets/YuAbout";
 export default {
   components: {
     yuHeader,
+    yuAbout,
     yuFooter
   },
   data: () => ({
@@ -49,4 +29,8 @@ export default {
 </script>
 
 <style>
+.container--about {
+  background-color: #f5f5f5;
+  height: fit-content;
+}
 </style>
---
+++ src/.vuepress/components/home/widgets/YuAbout.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <v-container>
+          <div class="about__title">About</div>
+          <p class="about__content">
+            Hello World. My name is Zhu Yu (朱瑜).
+            I come from China and currently work in Germany.
+            As a Frontend Developer my primary responsibility is that,
+            to understand the needs of user,
+            then rendering the information as friendly as possible.
+            <br />I recently work in automotive industry and responsible for frontend development of In-Vehicle Infotainment (IVI) system.
+            I am witnessing how software engineering is changing this traditional industry.
+            I feel so lucky to live in the age where technology is advancing at a rapid pace.
+            <br />In my free time, I also enjoy mobile app development by modern web technologies.
+            And I'm passionate about the applications of Machine-Learning in the field of computer vision as well.
+          </p>
+        </v-container>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "yuAbout"
+};
+</script>
+
+<style>
+* {
+  font-family: "Roboto", sans-serif;
+}
+
+.about__title {
+  margin-top: 40px;
+  font-size: 45px;
+  font-weight: lighter;
+  letter-spacing: 2px;
+}
+
+.about__content {
+  margin: 40px 0;
+  font-size: 14px;
+}
+</style>
