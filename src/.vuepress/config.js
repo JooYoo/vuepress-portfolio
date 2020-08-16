1dd2f3811cc256a3aed8c2b11cfc83de9b598767
JooYoo <jooyoo@outlook.com>
2020-08-16 13:08:42 +0200

    removed the sidebar of section-project and section-blog

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,36 +1,50 @@
 module.exports = {
   plugins: ['@vuepress/back-to-top'],
-  title: "Yu💻",
-  head:[
-    ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900' }],
-    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css' }],
+  title: 'Yu💻',
+  head: [
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css',
+      },
+    ],
   ],
-  description: "My awesome portfolio blog",
+  description: 'My awesome portfolio blog',
   themeConfig: {
     nav: [
-      { text: "Home", link: "/" },
-      { text: "Project", link: "/project/" },
-      { text: "Blog", link: "/blog/" },
+      { text: 'Home', link: '/' },
+      { text: 'Project', link: '/project/' },
+      { text: 'Blog', link: '/blog/' },
     ],
-    sidebar: {
-      '/blog/': [
-        '',    
-        'Article', 
-        'Article2',
-        'Article3'
-      ],
+    // sidebar: {
+    //   '/blog/': [
+    //     '',
+    //     'Article',
+    //     'Article2',
+    //     'Article3'
+    //   ],
 
-      '/project/': [
-        '',
-        'Project',
-        'Project2',
-        'Project3'
-      ],
+    //   '/project/': [
+    //     '',
+    //     'Project',
+    //     'Project2',
+    //     'Project3'
+    //   ],
 
-      '/': [
-        ''
-      ]
-    }
+    //   '/': [
+    //     ''
+    //   ]
+    // }
   },
   // plugins: [
   //   // "social-share",
