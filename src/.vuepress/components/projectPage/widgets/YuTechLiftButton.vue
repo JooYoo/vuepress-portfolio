f53c22d20681646ac8444aa4797662e2fadcdd40
jooyoo <jooyoo@outlook.com>
2020-09-02 10:56:29 +0200

    optimize liftButton

--- src/.vuepress/components/projectPage/widgets/YuTechLiftButton.vue
+++ src/.vuepress/components/projectPage/widgets/YuTechLiftButton.vue
@@ -1,15 +1,13 @@
 <template>
-  <div class="project-overview__card-footer-container">
+  <div class="lift-btn-container">
     <v-bottom-navigation
       v-for="tech in frontmatterTechs"
-      class="project-overview__card-footer-tech-tab"
+      class="lift-btn-tab"
       shift
+      :color="getColor(tech.name)"
     >
-      <v-btn>
-        <span
-          class="project-overview__card-footer-tech-tab--percent"
-          :style="{'border-color': getColor(tech.name)}"
-        >{{tech.percent}}</span>
+      <v-btn :style="setLogoColor(tech.name)">
+        <span class="lift-btn-tab-percent">{{tech.percent}}</span>
         <yuIcon :logo="tech.name"></yuIcon>
       </v-btn>
     </v-bottom-navigation>
@@ -39,23 +37,39 @@ export default {
     getColor: function (name) {
       return this.techs.find((obj) => obj.name == name).color;
     },
+
+    setLogoColor: function (name) {
+      return {
+        "--logo-color": this.getColor(name),
+      };
+    },
   },
 };
 </script>
 
 <style>
-.project-overview__card-footer-container {
+.lift-btn-container {
   display: flex;
   justify-content: center;
   width: 100%;
 }
 
-.project-overview__card-footer-tech-tab {
+.lift-btn-tab {
   box-shadow: unset !important;
   width: unset !important;
 }
 
-.project-overview__card-footer-tech-tab--percent {
-  border-bottom: 3px solid;
+/* .lift-btn-tab__button--active {
+  .yuIcon__display:active {
+    fill: red;
+  }
+} */
+
+.v-btn--active {
+  fill: var(--logo-color);
+}
+
+.lift-btn-tab-percent {
+  /* border-bottom: 3px solid; */
 }
 </style>
