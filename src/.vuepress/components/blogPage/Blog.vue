c984e0df620aa9be6abc5c4c5f097b86aecf2780
jooyoo <jooyoo@outlook.com>
2021-04-19 16:06:04 +0200

    feat: filter get post techs

--- src/.vuepress/components/blogPage/Blog.vue
+++ src/.vuepress/components/blogPage/Blog.vue
@@ -1,10 +1,11 @@
 <template>
   <v-list class="blog-list-container" two-line>
-    <v-chip-group v-model="selection" active-class="active" class="invisible">
-      <v-chip v-for="tag in tags" :key="tag">
-        {{ tag }}
+    <v-chip-group v-model="selection" active-class="active">
+      <v-chip v-for="(value, key) in uKeywords" :key="key">
+        {{ key }}
       </v-chip>
     </v-chip-group>
+    <v-divider></v-divider>
 
     <v-list-item-group>
       <router-link v-for="(page, index) in pages" :key="index" :to="page.path">
@@ -34,12 +35,13 @@
 </template>
 <script>
 export default {
-  // TODO: 1. get techs display as chips
+  // TODO: [x] 1. get techs display as chips
   // 2. chips color follow techs color
   // 3. click chip to get selection
   // 4. filter posts by selection
   data: () => ({
     pages: [],
+    uKeywords: {},
 
     // UI template
     selection: 'Work',
@@ -54,6 +56,17 @@ export default {
         this.pages.push(page);
       }
     });
+    // get unique keyword for each page
+    this.getUniqueKeywords();
+  },
+
+  methods: {
+    getUniqueKeywords: function () {
+      for (let page of this.pages) {
+        this.uKeywords[page.frontmatter.keyword] =
+          this.uKeywords[page.frontmatter.keyword] + 1 || 1;
+      }
+    },
   },
 };
 </script>
