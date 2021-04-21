a61039adc43aa1f0362c93e433b6d981def4d08c
jooyoo <jooyoo@outlook.com>
2021-04-21 05:25:45 +0200

    feat: filter click to select tech

--- src/.vuepress/components/blogPage/Blog.vue
+++ src/.vuepress/components/blogPage/Blog.vue
@@ -1,11 +1,15 @@
 <template>
   <v-list class="blog-list-container" two-line>
-    <v-chip-group v-model="selection" active-class="active">
+    <v-chip-group
+      v-model="selection"
+      class="blog-filter__chip-ground-container"
+    >
       <v-chip
         v-for="(value, key) in uKeywords"
         :key="key"
-        outlined
         :color="getTechColor(key)"
+        @click="getSelection(key)"
+        outlined
       >
         {{ key }}
       </v-chip>
@@ -21,9 +25,6 @@
               class="text--primary"
               v-text="page.frontmatter.keyword"
             ></v-list-item-subtitle>
-            <!-- <v-list-item-subtitle
-              v-text="page.frontmatter.description"
-            ></v-list-item-subtitle> -->
           </v-list-item-content>
 
           <v-list-item-action>
@@ -41,18 +42,16 @@
 <script>
 import JsonTechs from '../../data/techs.json';
 export default {
-  // TODO: [x] 1. get techs display as chips
-  // 2. chips color follow techs color
-  // 3. click chip to get selection
+  // TODO:
+  // [x] 1. get techs display as chips
+  // [x] 2. chips color follow techs color
+  // [x] 3. click chip to get selection
   // 4. filter posts by selection
   data: () => ({
     pages: [],
     uKeywords: {},
     techs: JsonTechs,
-
-    // UI template
-    selection: 'Work',
-    tags: ['Work', 'Home Improvement', 'Vacation', 'Food'],
+    selection: '',
   }),
 
   mounted() {
@@ -77,6 +76,10 @@ export default {
       const techColor = tech ? tech.color : '#68217a';
       return techColor;
     },
+    getSelection: function (name) {
+      this.selection = name;
+      console.log(this.selection);
+    },
   },
 };
 </script>
@@ -92,7 +95,7 @@ export default {
   padding-right: 0;
 }
 
-.invisible {
-  display: none;
+.blog-filter__chip-ground-container {
+  margin: 0 15px;
 }
 </style>
