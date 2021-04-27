71a8d3ec1ce47075c4c5969f32371e5a2f25d7cb
jooyoo <jooyoo@outlook.com>
2021-04-27 14:03:18 +0200

    refa: added education json

--- src/.vuepress/components/homePage/widgets/YuEducation.vue
+++ src/.vuepress/components/homePage/widgets/YuEducation.vue
@@ -3,14 +3,18 @@
     <div class="display-2 font-weight-thin training-title">Education</div>
 
     <v-card class="training-main-card">
-      <v-list subheader>
+      <v-list subheader v-for="(education, index) in educations" :key="index">
         <v-subheader inset>
           <v-list-item-title>
-            <span>Fachinformatiker - Anwendungsentwicklung | 2016 ~ 2019</span>
+            <span
+              >{{ education.degree }}&nbsp;|&nbsp;{{ education.timeRoom }}</span
+            >
             <div class="flag-container">
               <img
+                v-for="(flagUrl, index) in education.flagUrl"
+                :key="index"
                 class="flag-end"
-                src="https://www.flaticon.com/svg/static/icons/svg/197/197571.svg"
+                :src="flagUrl"
               />
             </div>
           </v-list-item-title>
@@ -18,113 +22,26 @@
 
         <v-list-item three-line>
           <v-list-item-action>
-            <span class="icon-start">👨🏻‍💻</span>
+            <span class="icon-start">{{ education.icon }}</span>
           </v-list-item-action>
           <v-list-item-content>
-            <v-list-item-title>Software Development</v-list-item-title>
-            <v-list-item-subtitle>Robert-Bosch-Schule Ulm</v-list-item-subtitle>
-            <v-list-item-subtitle>artiso solutions GmbH</v-list-item-subtitle>
-            <v-list-item-subtitle>IHK Ulm</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <v-divider></v-divider>
-
-      <v-list subheader>
-        <v-subheader inset>
-          <v-list-item-title>
-            <span>Master of Science | 2012 ~ 2014</span>
-
-            <div class="flag-container">
-              <img
-                class="flag-end"
-                src="https://www.flaticon.com/svg/static/icons/svg/197/197374.svg"
-              />
-              <img
-                class="flag-end"
-                src="https://www.flaticon.com/svg/static/icons/svg/197/197375.svg"
-              />
-            </div>
-          </v-list-item-title>
-        </v-subheader>
-
-        <v-list-item three-line>
-          <v-list-item-action>
-            <span class="icon-start">⏱</span>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Project Management</v-list-item-title>
-            <v-list-item-subtitle>University of Greenwich</v-list-item-subtitle>
+            <v-list-item-title>{{ education.major }}</v-list-item-title>
             <v-list-item-subtitle
-              >Yunnan University of Finance and Economics</v-list-item-subtitle
+              v-for="(studyAt, index) in education.studyAt"
+              :key="index"
+              >{{ studyAt }}</v-list-item-subtitle
             >
           </v-list-item-content>
         </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-      <v-list subheader>
-        <v-subheader inset>
-          <v-list-item-title>
-            <span>Bachelor of Business Studies | 2008 ~ 2012</span>
-            <div class="flag-container">
-              <img
-                class="flag-end"
-                src="https://www.flaticon.com/svg/static/icons/svg/197/197507.svg"
-              />
-              <img
-                class="flag-end"
-                src="https://www.flaticon.com/svg/static/icons/svg/197/197375.svg"
-              />
-            </div>
-          </v-list-item-title>
-        </v-subheader>
-        <v-list-item>
-          <v-list-item-action>
-            <span class="icon-start">👨🏻‍💼</span>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Business Studies</v-list-item-title>
-            <v-list-item-subtitle
-              >Charles Sturt University</v-list-item-subtitle
-            >
-            <v-list-item-subtitle
-              >Yunnan University of Finance and Economics</v-list-item-subtitle
-            >
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <v-divider></v-divider>
-      <v-list subheader>
-        <v-subheader inset>
-          <v-list-item-title>
-            <span>Bachelor of Management Studies | 2008 ~ 2012</span>
-            <div class="flag-container">
-              <img
-                class="flag-end"
-                src="https://www.flaticon.com/svg/static/icons/svg/197/197375.svg"
-              />
-            </div>
-          </v-list-item-title>
-        </v-subheader>
-        <v-list-item>
-          <v-list-item-action>
-            <span class="icon-start">💸</span>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Financial Management</v-list-item-title>
-            <v-list-item-subtitle
-              >Yunnan University of Finance and Economics</v-list-item-subtitle
-            >
-          </v-list-item-content>
-        </v-list-item>
+        <v-divider></v-divider>
       </v-list>
     </v-card>
   </v-container>
 </template>
 
 <script>
+import JsonEducations from '../../../data/educations.json';
+
 export default {
   name: 'yuEduction',
 
@@ -132,6 +49,7 @@ export default {
     notifications: false,
     sound: true,
     widgets: false,
+    educations: JsonEducations,
   }),
 };
 </script>
